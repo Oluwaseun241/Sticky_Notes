8abdefd7a22cc5e184a8340b439fc36673ae775d
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-16 22:11:21 +0100

    models: migrate

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class StickyNote(models.Model):
+    note = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
