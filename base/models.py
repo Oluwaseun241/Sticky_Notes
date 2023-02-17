8decba6cac09fc39de798e5e029080ac7a073d3a
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-17 12:12:21 +0100

    template: add-notes

--- base/models.py
+++ base/models.py
@@ -5,3 +5,6 @@ class StickyNote(models.Model):
     note_text = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.note_text
--- base/views.py
+++ base/views.py
@@ -3,6 +3,9 @@ from .models import StickyNote
 
 # Create your views here.
 def home(request):
-    notes = StickyNote.objects.get(id=1)
+    notes = StickyNote.objects.all()
     context = {'notes': notes}
     return render(request, 'base.html', context)
+
+def create(request):
+    return render(request, 'form.html')
