6db946ccc0a7254656ccd082438f652df5eb5ed2
Oluwaseun <tanimolaoluwaseun70@gmail.com>
2023-02-23 21:07:29 +0100

    fix: Ui

--- base/models.py
+++ base/models.py
@@ -5,7 +5,7 @@ class StickyNote(models.Model):
     note_text = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now_add=True)
-    background_color = models.CharField(max_length=255, default='#FFFFFF')
+    background_color = models.CharField(max_length=255)
 
     def __str__(self):
         return self.note_text
